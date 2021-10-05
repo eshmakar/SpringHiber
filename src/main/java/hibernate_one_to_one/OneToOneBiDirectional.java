43310c2a213a600d2ce64caa615ba2c187de10bf
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-10-06 00:17:19 +0300

    @OneToMany

--- src/main/java/hibernate_one_to_one/OneToOneBiDirectional.java
+++ src/main/java/hibernate_one_to_one/OneToOneBiDirectional.java
@@ -1,7 +1,7 @@
-package hibernate_test2;
+package hibernate_one_to_one;
 
-import hibernate_test2.entity.Detail;
-import hibernate_test2.entity.Employee;
+import hibernate_one_to_one.entity.Detail;
+import hibernate_one_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -37,7 +37,7 @@ public class OneToOneBiDirectional {
 
             session.beginTransaction();
             Detail d = session.get(Detail.class, 4);
-            d.getEmployee().setEmpDetail(null);//без этого обрывания связи данные не будут удалены
+            d.getEmployee().setEmpDetail(null);//без этого обрывания связи, данные не будут удалены
             session.delete(d);
             session.getTransaction().commit();
 
